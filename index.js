25f1e57c11b423067c13ad30fb6b1776bb94a449
Anshorimuslim <ans4175@gmail.com>
2021-11-15 02:21:40 +0700

    feat(express): add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const RandomGoFood = require('./RandomGoFood');
+const cors = require('cors')
 
 app.use(express.json({ extended: false}));
+app.use(cors());
 app
     .get('/', async (req, res) => {
         res.json({
